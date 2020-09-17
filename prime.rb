b44f023ffaca4b36c7efbcbac17bd31ad1069a22
Alan Young <alanryoung01@gmail.com>
2020-09-17 08:13:31 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(value)
+chainstart = 2
+if value > 1
+  primes = (start..number-1).to_a
+  primes.none? do |test_number|
+    value % test_number == 0
+  end
+else
+  false
+end
+end
